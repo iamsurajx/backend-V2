aae944d4e7bd3e55ec0ec12c0c94e85ce3a58680
tanmay2809 <tanmay2809@outlook.com>
2024-12-22 18:26:38 +0530

    dd

--- models/experience.js
+++ models/experience.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 const ExperienceSchema = new mongoose.Schema(
   {
-    title: { type: String, required: true }, // Job title or role
-    company: { type: String, required: true }, // Company or organization name
-    startDate: { type: Date, required: true }, // Start date of the experience
-    endDate: { type: Date }, // End date of the experience (optional)
+    title: { type: String }, // Job title or role
+    company: { type: String }, // Company or organization name
+    startDate: { type: String }, // Start date of the experience
+    endDate: { type: String }, // End date of the experience (optional)
     description: { type: String }, // Description of the role and responsibilities
     location: { type: String }, // Location of the job (e.g., city, country)
     skills: { type: [String] }, // Array of skills used or gained in this role
