d0ad43ac96735b863de72a912c27eeb39ab572cf
Ventur.Loop <we.venturloop@gmail.com>
2024-12-26 10:26:32 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,10 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const connectDB = () => {
-  mongoose.connect("mongodb+srv://connect:SYo1XnmCJWVgVSys@cluster0.b57i5.mongodb.net/", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true, // Helps to avoid deprecated warnings
-  });
+  mongoose.connect("mongodb+srv://connect:SYo1XnmCJWVgVSys@cluster0.b57i5.mongodb.net/");
 
   mongoose.connection
     .once("open", function () {
